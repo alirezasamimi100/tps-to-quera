d516389ef1d2be3634002ed8e49257b391b6f90e
Alireza Samimi <alirezasamimi100@yahoo.com>
2022-08-31 09:20:41 +0430

    Closing files manually

--- BuildConfig.cpp
+++ BuildConfig.cpp
@@ -10,18 +10,21 @@ int main(){
     ifstream js("subtasks.json");
     ofstream qj("config.json");
     json subs = json::parse(js), qu;
+    js.close();
     string s;
     int x;
     while(mi>>s){
         mi >> x;
         mp[s].push_back(x);
     }
+    mi.close();
     qu["packages"] = json::array();
     for(auto it = subs["subtasks"].begin(); it != subs["subtasks"].end(); ++it){
         s = it.key();
         qu["packages"].push_back({ {"score", it.value()["score"]}, {"tests", mp[s]} });
     }
     qj << qu.dump(4);
+    qj.close();
     return 0;
 }
 
